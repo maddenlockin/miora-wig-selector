d381cb0ee000749530ec6798dfef004a49f0f199
maddenlockin <maddenlockin@gmail.com>
2022-10-27 16:26:50 -0700

    add wig change counter div and grab all DOM elements for wig select and display

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const wigSelect = document.getElementById('wig-select');
+const wigImg = document.getElementById('wig-img');
+const wigChangeCounter = document.getElementById('wig-change-counter');
 /* State */
 
 /* Events */
