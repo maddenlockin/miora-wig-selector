f125176e024ac134a0afe9f1cdd50a31d0e65483
maddenlockin <maddenlockin@gmail.com>
2022-10-27 17:29:43 -0700

    add eventListener for wig select: change img src and display change counter incrementing

--- app.js
+++ app.js
@@ -4,10 +4,24 @@
 const wigSelect = document.getElementById('wig-select');
 const wigImg = document.getElementById('wig-img');
 const wigChangeCounter = document.getElementById('wig-change-counter');
+
 /* State */
+let wigCounter = 0;
 
 /* Events */
+wigSelect.addEventListener('change', (e) => {
+    // on change, grab the user selection and save to a variable
+    const value = e.target.value;
+    // apply value to img src with template literal
+    wigImg.src = `./assets/${value}.png`;
+    //increment wigCounter by 1 for the one change
+    wigCounter++;
+    displayStats();
+});
 
 /* Display Functions */
+function displayStats() {
+    wigChangeCounter.textContent = wigCounter;
+}
 
 // (don't forget to call any display functions you want to run on page load!)
