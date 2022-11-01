1334e368e43b8cede0d8d15b4a279c5c4f4fdd2c
maddenlockin <maddenlockin@gmail.com>
2022-11-01 16:15:44 -0700

    create wig select and functionality

--- app.js
+++ app.js
@@ -5,56 +5,11 @@ const wigSelect = document.getElementById('wig-select');
 const wigImg = document.getElementById('wig-img');
 const wigChangeCounter = document.getElementById('wig-change-counter');
 
-const locationSelect = document.getElementById('location-select');
-const locationImg = document.getElementById('location-img');
-const locationChangeCounter = document.getElementById('location-counter');
-
-const aphorismInput = document.getElementById('aphorism-input');
-const addButton = document.getElementById('add-button');
-const aphorismList = document.getElementById('aphorism-list');
-
-/* State */
 let wigCounter = 0;
-let locationCounter = 0;
-let aphorisms = [];
 
-/* Events */
 wigSelect.addEventListener('change', (e) => {
-    // on change, grab the user selection and save to a variable
     const value = e.target.value;
-    // apply value to img src with template literal
     wigImg.src = `./assets/${value}.png`;
-    //increment wigCounter by 1 for the one change
     wigCounter++;
-    displayStats();
+    wigChangeCounter.textContent = wigCounter;
 });
-
-locationSelect.addEventListener('change', (e) => {
-    const value = e.target.value;
-    locationImg.src = `./assets/${value}.png`;
-    locationCounter++;
-    displayStats();
-});
-
-addButton.addEventListener('click', () => {
-    aphorisms.push(aphorismInput.value);
-    displayAphorisms();
-    aphorismInput.value = '';
-});
-
-/* Display Functions */
-function displayStats() {
-    wigChangeCounter.textContent = `You have changed wigs ${wigCounter} times`;
-    locationChangeCounter.textContent = `You have changed locations ${locationCounter} times`;
-}
-
-function displayAphorisms() {
-    aphorismList.textContent = '';
-    for (let aphorism of aphorisms) {
-        const p = document.createElement('p');
-        p.textContent = aphorism;
-        aphorismList.append(p);
-    }
-}
-
-// (don't forget to call any display functions you want to run on page load!)
